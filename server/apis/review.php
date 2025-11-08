1e0947280f87428d5e20be957dae65bbbe49f65b
Mohmmad Rostom <mohammadrstm2004@gmail.com>
2025-11-08 04:42:10 +0200

    implemented the review api which can read either a file or a code snippet and call the openAI service to get a response

---
+++ server/apis/review.php
@@ -0,0 +1,29 @@
+<?php
+// Human to AI comparison
+
+// include error handling file
+include "../services/handleRequestError.php";
+include "../services/callOpenAI.php";
+
+// reveive data from client
+$data = json_decode(file_get_contents('php://input'), true);
+
+if(!isset($data["code"]) && !isset($_FILES["file"])){
+    handleRequestError("You must atleast include either the file or the code snippet" , 401);
+}else if(isset($data["code"]) && isset($_FILES["file"])){
+    handleRequestError("Both code snippet and code file can't be included, choose one at a time to review" , 401);
+}else if(isset($_FILES["file"]) && $_FILES["file"]["error"] === UPLOAD_ERR_OK){
+    $tmp_file = $_FILES["file"]["tmp_name"];
+    $fileCode = file_get_contents($tmp_file); 
+    $response = reviewCode( $fileCode, $_FILES["file"]["name"]);
+}else if(isset($data["code"])){
+    $code = $data["code"];
+    $response = reviewCode($code);
+}
+
+if(isset($response["error"])){
+    echo "FAIL : ". $response["error"];
+}else{
+    echo $response;
+}
+?>
